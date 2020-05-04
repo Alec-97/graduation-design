a14079b2e55db4c21797afb019faba3bfe023892
Alec-97 <61488598+Alec-97@users.noreply.github.com>
2020-05-04 16:57:36 +0800

    Update get_color.py

--- SVM/SVM-SGD/GetData/get_color.py
+++ SVM/SVM-SGD/GetData/get_color.py
@@ -2,14 +2,14 @@ import datetime
 import pandas as pd
 
 
-def get_color(inputpath):
-    print('Excel File Path: {}'.format(inputpath))
-    start = datetime.datetime.now()
+def get_color(inputpath):  #定义get_color函数
+    print('Excel File Path: {}'.format(inputpath))  #打印显示表格的路径
+    start = datetime.datetime.now()  #赋值当前系统的时间
     # return a dict {sheet_name:dataframe}
-    excel_file = pd.read_excel(inputpath, sheet_name=None)
-    end = datetime.datetime.now()
-    print('Read Excel Time: {}'.format(end - start))
-    sheet_num = len(excel_file)
+    excel_file = pd.read_excel(inputpath, sheet_name=None) #从指定的路径中读取表格到excel_file
+    end = datetime.datetime.now() #赋值系统的当前时间
+    print('Read Excel Time: {}'.format(end - start)) #打印读取文件所用的时间
+    sheet_num = len(excel_file) #读出表格的长度
     sheet_names = list(excel_file.keys())  # dict_keys->list
 
     sheets = [0]*sheet_num
